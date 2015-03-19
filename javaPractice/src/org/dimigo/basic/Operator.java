03a11a9b33e92b80c053c8c777e8565065d2a5e7
namjeongsuyong <namjeongsuyong@172.17.3.83>
2015-03-19 15:33:05 +0900

    3번째 실습과

---
+++ javaPractice/src/org/dimigo/basic/Operator.java
@@ -0,0 +1,26 @@
+package org.dimigo.basic;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		int a = 9,b=10;
+		double sum1,sum2,sum3;
+		double d1 = 5.8;
+		double d2 = 5.4;
+		
+		
+		sum1 = ((a+b)*d1/2);
+		sum2 = (a*d2);
+		sum3 = sum1 - sum2;
+		
+		System.out.println("<<도형 넓이 비교>>");
+		System.out.println("사다리꼴 넓이:"+sum1);
+		System.out.println("평행사변형의 넓이:"+sum2);
+		
+		System.out.println("사다리꼴이 평행사변형보다"+sum3+"더 큽니다.");
+		
+		
+
+	}
+
+}

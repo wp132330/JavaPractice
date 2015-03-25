642bd9385aa071df18bdd10121d14f00a378d42b
namjeongsuyong <namjeongsuyong@172.17.3.83>
2015-03-25 16:11:14 +0900

    4번째 실습과제입니다.

--- javaPractice/src/org/dimigo/basic/Condition.java
+++ javaPractice/src/org/dimigo/basic/Condition.java
@@ -3,26 +3,25 @@ package org.dimigo.basic;
 public class Condition {
 
 	public static void main(String[] args) {
-		int distance = 15;
-		String car="경 차";
-		int money = 0;
+		String car="그 외";
+		int distance = 25;
+		int money=0;
 		
 		switch(car){
 		case "고속버스":
-			money = 850+((distance-1)/10)*300;
+			money=850+((distance-1)/10)*300;
 			break;
-		case "경 차":
-			money = 300+((distance-1)/10)*200;
+		case "경차":
+			money=300+((distance-1)/10)*200;
 			break;
-		case "그 외":
-			money = 600+((distance-1)/10)*200;
-			break;
-		}
+		default:
+			money=600+((distance-1)/10)*200;
 		
-	
+		}
 		System.out.println("<<고속도로 통행료 계산>>");
-		System.out.println("거리 : "+distance+"Km");
-		System.out.println("차종 : "+car);
-		System.out.println("통행료 : "+money+"원");
+		System.out.println("거리 :"+ distance+"Km");
+		System.out.println("차종 :"+ car);
+		System.out.println("통행료 :"+money+"원");
 	}
+
 }

7c00752aaa7e442328192128de45502fe4480f55
namjeongsuyong <namjeongsuyong@172.17.3.83>
2015-03-25 14:15:56 +0900

    4번째 과제입니다.

---
+++ javaPractice/src/org/dimigo/basic/Condition.java
@@ -0,0 +1,26 @@
+package org.dimigo.basic;
+
+public class Condition {
+
+	public static void main(String[] args) {
+		int distance = 10;
+		String car="고속버스";
+		int money;
+		
+		switch(car){
+		case "고속버스":
+			money = 850+((distance-1)/10*300);
+			break;
+		case "경차":
+			money = 300+((distance-1)/10*200);
+		default:
+			money = 600+((distance-1)/10*200);
+		}
+		
+	
+		System.out.println("<<고속도로 통행료 계산>>");
+		System.out.println("거리 : "+distance+"Km");
+		System.out.println("차종 : "+car);
+		System.out.println("통행료 : "+money+"원");
+	}
+}

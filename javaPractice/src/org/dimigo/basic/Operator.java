1d145a50d7211194710699fab001bb04be58f75a
wp132330 <suyong227@gmail.com>
2015-03-25 15:56:54 +0900

    3번째 실습과제

--- javaPractice/src/org/dimigo/basic/Operator.java
+++ javaPractice/src/org/dimigo/basic/Operator.java
@@ -3,7 +3,7 @@ package org.dimigo.basic;
 public class Operator {
 
 	public static void main(String[] args) {
-		int a = 9,b=10;
+		int a = 9,b=10,c = 0;
 		double sum1,sum2;
 		double d1 = 5.8;
 		double d2 = 5.4;
@@ -17,17 +17,19 @@ public class Operator {
 			sum3 = sum1 - sum2;
 		}
 		else{
+			sum1=c;
+			c=(int) sum1;
 			sum1=sum2;
-			sum2=sum1;
+			sum2=c;
+			
 			sum3 = sum1 - sum2;
 			
 		}
+		
 		System.out.println("<<도형 넓이 비교>>");
 		System.out.println("사다리꼴 넓이:"+sum1);
 		System.out.println("평행사변형의 넓이:"+sum2);
-		
 		System.out.println("사다리꼴이 평행사변형보다"+sum3+"더 큽니다.");
-
 	}
 
 }

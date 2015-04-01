0750735f2f40faeca9249d74a7a4b689ccb0351e
namjeongsuyong <namjeongsuyong@172.17.3.83>
2015-04-01 13:55:52 +0900

    5번째 과제

--- javaPractice/src/org/dimigo/basic/Loop.java
+++ javaPractice/src/org/dimigo/basic/Loop.java
@@ -3,7 +3,7 @@ package org.dimigo.basic;
 public class Loop {
 
 	public static void main(String[] args) {
-		for(int a=1; a <=10;a++)
+		for(int a=1; a<=10; a++)
 		{
 			for(int b=1; b<=10; b++){
 				if(b==a)

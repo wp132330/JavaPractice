cbe566074d569fd7703d183a38dbf63584a5344e
namjeongsuyong <namjeongsuyong@172.17.3.83>
2015-04-01 14:48:05 +0900

    5번째과제

---
+++ JavaPractice/src/org/dimigo/basic/Loop.java
@@ -0,0 +1,18 @@
+package org.dimigo.basic;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		for(int a=1; a<=10; a++)
+		{  
+			for(int b=1; b<=10; b++){
+				if(b==a)
+					System.out.print("*");
+				else
+					System.out.print(b);
+			}
+			System.out.println();
+		}
+	}
+
+}

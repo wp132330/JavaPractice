3ff89a68c6e690d40b7e142e07d53c1f088218ee
namjeongsuyong <namjeongsuyong@172.16.1.213>
2015-05-26 22:11:15 +0900

    과제

---
+++ JavaPractice/src/org/dimigo/oop/Snack.java
@@ -0,0 +1,69 @@
+package org.dimigo.oop;
+
+public class Snack {
+	private String name;
+	private String company;
+	private int price;
+	private int number;
+	
+	public Snack(){
+		
+	}
+	
+public Snack(String name, String company, int price, int number){
+		
+		this.name = name;
+		this.company = company;
+		this.price = price;
+		this.number = number;
+	}
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public void printSnack() {
+		System.out.println("이름 : " + getName());
+		System.out.println("제조사 : " +  getCompany());
+		System.out.println("가격 : " + String.format("%,d", getPrice()) + "원" );
+		System.out.println("개수 : " + getNumber() + "개");
+	}
+	
+	public int calcPrice() {
+		int total = 0;
+		total += getNumber() * getPrice();
+		return total;
+	}
+	
+
+}
